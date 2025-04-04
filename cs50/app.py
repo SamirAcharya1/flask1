10215dd4ff5064cb164de2697af5789e23d2b105
SamirAcharya1 <158140847+SamirAcharya1@users.noreply.github.com>
2025-04-04 11:33:56 +0000

    Database support added.

--- cs50/app.py
+++ cs50/app.py
@@ -1,39 +1,44 @@
 from flask import Flask, render_template, request, redirect
+from cs50 import SQL
 
 app = Flask(__name__)
 
+db = SQL("sqlite:///registrants.db")
+
 SPORTS = ["Basketball", "Soccer", "Ultimate Frisbee"]
 
-REGISTRANTS = {
-    
-}
+REGISTRANTS = {}
 
 @app.route("/",)
 def index():
     return render_template("register.html", sports=SPORTS)
 
-@app.route("/register", methods=["POST"])
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    if not request.form.get("name") or not request.form.get("sport"):
-        return "Invalid!!"
-    else:
+    if request.method == "POST":
         name = request.form.get("name")
         sport = request.form.get("sport")
-        REGISTRANTS[name] = sport
-        
-    return redirect("/list")
+        if not name or not sport in SPORTS:
+            return "Invalid!!"
+        else:
+            db.execute("INSERT INTO registrants(name, sports) VALUES(?, ?)", name, sport)
+            
+        return redirect("/list")
+    
+    return redirect("/")
     
 
 @app.route("/list")
 def list():
-    return render_template("list.html", registrants=REGISTRANTS)
+    rows = db.execute("SELECT * FROM registrants")
+    return render_template("list.html", registrants=rows)
 
 @app.route("/deregister", methods=["POST"])
 def deregister():
     if request.form.get("id"):
         try:
-            del REGISTRANTS[request.form.get("id")]
-        except KeyError:
+            db.execute("DELETE FROM registrants WHERE id = ?", request.form.get("id"))
+        except TypeError:
             return "No data with the id found!!"
     else:
         return "Invalid!!"
