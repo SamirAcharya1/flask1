761a9a25178459a4f9f8da80373c35d1ad2e5a1d
SamirAcharya1 <158140847+SamirAcharya1@users.noreply.github.com>
2025-04-07 12:19:21 +0000

    Added the update functionality

--- cs50/app.py
+++ cs50/app.py
@@ -44,3 +44,29 @@ def deregister():
         return "Invalid!!"
     
     return redirect("/list")
+
+@app.route("/update", methods=["Post"])
+def update():
+    if request.form.get("id"):
+        try:
+            data = db.execute("SELECT * FROM registrants WHERE id = ?", request.form.get("id"))
+        except TypeError:
+            return "No data with the id found!!"
+    else:
+        return "Invalid!!"
+    
+    return render_template("update.html", data=data[0], sports=SPORTS)
+
+@app.route("/updatedata", methods=["POST"])
+def updatedata():
+    if request.method == "POST":
+        id = request.form.get("id")
+        name = request.form.get("name")
+        sport = request.form.get("sport")
+        if not id or not name or not sport in SPORTS:
+            return "Invalid!!"
+        else:
+            db.execute("UPDATE registrants SET name = ?, sports = ? WHERE id = ?", name, sport, id)
+            return redirect("/list")
+
+    return redirect("/")
